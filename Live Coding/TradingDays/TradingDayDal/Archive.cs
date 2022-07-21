cc659573d86777e7af31438039798e99798fae10
Olaf Lischke <olaf.lischke@lischke-edv.de>
2022-07-21 10:17:46 +0200

    TradingDay-Sample

---
+++ Live Coding/TradingDays/TradingDayDal/Archive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TradingDayDal
+{
+    public class Archive
+    {
+        public Archive(string url)
+        {
+            this.TradingDays = GetData(url);
+        }
+
+        private List<TradingDay>? GetData(string url)
+        {
+            XDocument xmlDoc=XDocument.Load(url);
+
+            var qDays = xmlDoc.Root.Descendants()
+                .Where(xe => xe.Name.LocalName == "Cube" && xe.Attributes().Any(at => at.Name == "time"))
+                // Projektion
+                .Select(xe => new TradingDay(xe));
+
+            return qDays.ToList();
+        }
+
+        public List<TradingDay>? TradingDays { get; set; }
+    }
+}
---
+++ Live Coding/TradingDays/TradingDayDal/TradingDay.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace TradingDayDal
+{
+    public class TradingDay
+    {
+        public TradingDay(XElement tradingDayNode)
+        {
+            this.Date = DateOnly.Parse(tradingDayNode.Attribute("time").Value);
+
+            this.ExchangeRates = tradingDayNode.Elements().Select(el => new ExchangeRate()
+                                                                        {
+                                                                            EuroRate = Convert.ToDouble( el.Attribute("rate").Value),
+                                                                            Symbol = el.Attribute("currency").Value
+                                                                        })
+                                                            .ToList();
+        }
+
+        public DateOnly Date { get; set; }
+        public List<ExchangeRate> ExchangeRates { get; set; }
+
+    }
+}
